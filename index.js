0cf7d723e49013ac612683defde955001b7fdceb
xcstream <cheng.xu@all-dream.com>
2018-11-09 15:15:58 +0800

    init

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 var port = 7000
 var express= require('express')
 
-
-
+const serveStatic = require('serve-static')
+const static = serveStatic('public' )
+const session = require('express-session')
+const bodyParser = require("body-parser");
 var app = express()
+sessionSetting = {
+    secret: 'hpan',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        secure: false,
+        maxAge: 1000 * 3600 * 24 * 30
+    },
+}
+app.use(bodyParser.json({ type: 'application/json' }))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session(sessionSetting))
 
-
-
+app.use(static);
 
 app.listen(port)
 console.log(`http://localhost:${port}/`)
